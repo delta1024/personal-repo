8328fcffcc294410cb5189256efddb06b7585601
Jacob Stannix <jakestannix at gmail dot com>
2021-05-22 18:02:18 -0600

    repo updates

--- update-repo.sh
+++ update-repo.sh
@@ -4,6 +4,7 @@ cd x86_64
 repo-add $REPO_NAME.db.tar.gz *.pkg.tar.zst
 mv $REPO_NAME.db.tar.gz $REPO_NAME.db
 mv $REPO_NAME.files.tar.gz $REPO_NAME.files
+git add *.pkg.tar.zst
 git add -u
 git commit -m "repo updates"
 git push -u
